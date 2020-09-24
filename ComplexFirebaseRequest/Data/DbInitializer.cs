6f733a9bcce44a267998bcfda9a92b2773e9bd7d
3phase <pbozhinow@gmail.com>
2020-09-24 18:22:14 +0300

    Pushes app's database connection and data seeder

---
+++ ComplexFirebaseRequest/Data/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using ComplexFirebaseRequest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplexFirebaseRequest.Data
+{
+    public class DbInitializer
+    {
+
+        public static void Initialize(DatabaseContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var users = new User[]
+            {
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 1, Role = 0 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 1, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 1, Role = 2 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 0 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 2 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 2, Role = 2 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 3, Role = 0 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 3, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 3, Role = 1 },
+                new User{Uid = Guid.NewGuid().ToString(), Domain = 3, Role = 1 },
+            };
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ ComplexFirebaseRequest/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplexFirebaseRequest.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Uid { get; set; }
+        public int Domain { get; set; }
+        // roles are going to be 0 - admin, 1 - user, 2 - limited
+        public int Role { get; set; }
+    }
+}
