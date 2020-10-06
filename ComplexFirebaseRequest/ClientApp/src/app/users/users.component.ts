93a8bb7f7d1e715c967ceefb1d801f8189ef15fa
3phase <pbozhinow@gmail.com>
2020-10-06 20:55:02 +0300

    Adds user component

---
+++ ComplexFirebaseRequest/ClientApp/src/app/users/users.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+
+  public localUserData: [];
+
+  constructor(
+    public service: UsersService
+  ) { }
+
+  
+  public fetchData() {
+    this.service.fetchData().subscribe((data: []) => {
+      this.localUserData = data;
+    });
+  }
+
+}
